25aeff092f74c4ef0951fa56bd0c1d5429ef3931
zjf <zjf@qq.com>
2022-04-29 15:26:01 +0800

    first commit

---
+++ src/main/java/com/zjf/boot/ProSpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.zjf.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSpringbootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProSpringbootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjf/boot/ProSpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjf.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSpringbootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
